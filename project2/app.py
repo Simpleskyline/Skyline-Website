95d531c30993aae170e4e2941b91f03a4cfff9c8
skyline <telvintran@gmail.com>
2025-08-25 15:01:05 +0300

    FLASK PROJECT

--- project2/app.py
+++ project2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,7 +16,20 @@ class Todo(db.Model):
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
-    return render_template("index.html")
+    if request.method =="POST":
+        task_content == request.form['content']
+        new_task == Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There wa an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+         return render_template("index.html", task = task)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
